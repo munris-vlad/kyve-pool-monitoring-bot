27f64cc9ad7ca7715f593564b452a1eadbe324fd
munris <>
2022-11-09 01:28:29 +0200

    test handler

--- app/Services/TelegramMessageHandleService.php
+++ app/Services/TelegramMessageHandleService.php
@@ -2,24 +2,21 @@
 
 namespace App\Services;
 
-use Telegram\Bot\Actions;
-use Telegram\Bot\Commands\Command;
 use Telegram\Bot\Exceptions\TelegramSDKException;
-use Telegram\Bot\Laravel\Facades\Telegram;
+use Telegram\Bot\Methods\Message;
 
 class TelegramMessageHandleService
 {
+    use Message;
 
     /**
      * @throws TelegramSDKException
      */
     public function handle($updates): bool
     {
-        $telegram = new Telegram;
         $chatId = $updates['message']['chat']['id'];
         $text = $updates['message']['text'];
-        $telegram->sendChatAction([
-            'action' => Actions::TYPING,
+        $this->sendMessage([
             'chat_id' => $chatId,
             'text' => 'Your valoper: '.$text
         ]);
