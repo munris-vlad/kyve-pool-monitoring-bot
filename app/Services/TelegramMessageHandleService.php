db649bc542b3399b85b7eb53b11ad3e19be35ed6
munris <>
2022-11-09 01:53:17 +0200

    test save valoper

--- app/Services/TelegramMessageHandleService.php
+++ app/Services/TelegramMessageHandleService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use App\Models\UserValoper;
 use Telegram\Bot\Exceptions\TelegramSDKException;
 use Telegram\Bot\Laravel\Facades\Telegram;
 use Telegram\Bot\Methods\Message;
@@ -20,10 +21,34 @@ class TelegramMessageHandleService
         $chatId = $updates['message']['chat']['id'];
         $text = $updates['message']['text'];
 
+        if (!str_contains($text, 'valoper') || strlen($text) !== 43) {
+            $telegram->sendMessage([
+                'chat_id' => $chatId,
+                'text' => 'Please send a valid validator address.'
+            ]);
+
+            return true;
+        }
+
+        if (UserValoper::where('chatId', $chatId)->where('valoper', $text)->count()) {
+            $telegram->sendMessage([
+                'chat_id' => $chatId,
+                'text' => 'You have already subscribed to this validator.'
+            ]);
+
+            return true;
+        }
+
+        UserValoper::create([
+            'chatId' => $chatId,
+            'valoper' => $text
+        ]);
+
         $telegram->sendMessage([
             'chat_id' => $chatId,
-            'text' => 'Your valoper: '.$text
+            'text' => 'Your valoper: '.$text.' successfully added to monitoring list',
         ]);
+
         return true;
     }
 }
